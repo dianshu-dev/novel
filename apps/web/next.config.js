a53de202a4f1d8d6152b63aa349ac6188b394de8
songhandong <songhandong@baidu.com>
2023-09-08 14:49:39 +0800

    feat: 允许跨域CORS

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -45,6 +45,31 @@ const nextConfig = {
       },
     ];
   },
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // 使用通配符 * 允许所有来源
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+      },
+    ];
+  },
   productionBrowserSourceMaps: true,
 };
 
